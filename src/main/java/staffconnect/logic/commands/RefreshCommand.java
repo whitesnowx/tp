9eb656dedb4a3a6233b1728c377532d53692bc3a
JerryWang0000 <jerrywang0200@gmail.com>
2024-04-02 14:19:08 +0800

    Update RefreshCommand.java to substantiate the CommandResult

--- src/main/java/staffconnect/logic/commands/RefreshCommand.java
+++ src/main/java/staffconnect/logic/commands/RefreshCommand.java
@@ -24,34 +24,49 @@ public class RefreshCommand extends Command {
     public static final String REFRESH_SUCCESS = "Upcoming meetings successfully refreshed. "
             + "All outdated meetings are deleted.";
 
+    public static final String REFRESH_NO_MODIFICATION = "Upcoming meetinns successfully refreshed. "
+            + "No meetings are outdated.";
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
         LocalDateTime currentTime = LocalDateTime.now();
-
         List<Person> lastShownList = model.getSortedFilteredPersonList();
+
+        boolean isAnyPersonModified = false;
+        String deletedMeetings = "";
 
         for (int i = 0; i < lastShownList.size(); i++) {
             Person person = lastShownList.get(i);
             Person newPerson = PersonUtil.copyPerson(person);
             List<Meeting> meetingShownList = newPerson.getFilteredMeetings();
             List<Meeting> toDelete = new ArrayList<>();
+            boolean isModified = false;
 
             for (Meeting meeting : meetingShownList) {
                 if (meeting.getStartDate().getDateTime().isBefore(currentTime)) {
                     toDelete.add(meeting);
+                    isModified = true;
+                    isAnyPersonModified = true;
+                    deletedMeetings += newPerson.getName() + String.format(" (Index: %d) %s\n", i+1, meeting);
                 }
             }
 
             for (Meeting meeting : toDelete) {
                 newPerson.removeMeeting(meeting);
             }
-            newPerson.updateSortedMeetingList(MEETING_DATE_THEN_DESCRIPTION_COMPARATOR);
-            model.setPerson(person, newPerson);
+            if (isModified) {
+                newPerson.updateSortedMeetingList(MEETING_DATE_THEN_DESCRIPTION_COMPARATOR);
+                model.setPerson(person, newPerson);
+            }
         }
-
-        return new CommandResult(REFRESH_SUCCESS);
+        if (isAnyPersonModified) {
+            return new CommandResult(String.format("%s\n%s\n%s", REFRESH_SUCCESS,
+                    "The following meetings are deleted", deletedMeetings));
+        } else {
+            return new CommandResult(REFRESH_NO_MODIFICATION);
+        }
     }
 
     @Override
--- src/test/java/staffconnect/logic/commands/RefreshCommandTest.java
+++ src/test/java/staffconnect/logic/commands/RefreshCommandTest.java
@@ -1,5 +1,21 @@
 package staffconnect.logic.commands;
 
+import org.junit.jupiter.api.BeforeEach;
+import staffconnect.model.Model;
+import staffconnect.model.ModelManager;
+import staffconnect.model.UserPrefs;
+
+import static staffconnect.testutil.TypicalPersons.getTypicalStaffBook;
+
 public class RefreshCommandTest {
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalStaffBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getStaffBook(), new UserPrefs());
+    }
+
 
 }
